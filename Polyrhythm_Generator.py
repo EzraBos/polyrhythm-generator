70dd72b0cd354f2a75539afde15eeafd924d2f32
EzraBos <97925988+EzraBos@users.noreply.github.com>
2022-05-08 17:29:54 -0400

    Add files via upload

--- Polyrhythm_Generator.py
+++ Polyrhythm_Generator.py
@@ -1,6 +1,6 @@
-import threading
+#import threading
 import time
-import winsound
+#import winsound
 #from playsound import playsound
 import pygame
 
@@ -10,25 +10,7 @@ beep2 = pygame.mixer.Sound("beep2.wav")
 channel1 = pygame.mixer.Channel(0)
 channel2 = pygame.mixer.Channel(1)
 
-def speed_func(ratio, tempo):
-    return (tempo - (tempo / ratio)) / tempo
- 
-#channel1.play(beep1, loops = -1)
 
-def pulse(beep_num, speed):
-    my_speed = speed_func(speed, 120)
-    #time.sleep(my_speed)
-    #time.sleep(my_speed)
-    #playsound(f"beep{beep_num}.mp3") # beep1.mp3 if beep_num is 1
-    # winsound.PlaySound(f"beep{beep_num}.wav", winsound.SND_ASYNC)
-    channel2.play(beep2, loops = -1)
-
-# while True:
-#     my_speed1 = speed_func(2, 120)
-#     my_speed2 = speed_func(2, 120)
-#     pulse(1, 2)
-#     #pulse(2, 3)
-#     time.sleep(.5)
 
 def maybe_play(current_time):
     print(current_time)
@@ -67,49 +49,63 @@ while True:
     time.sleep(0.1)
     
 
+# Different failed variations to accomplish the same goal 
+
+def speed_func(ratio, tempo):
+     return (tempo - (tempo / ratio)) / tempo
+ 
+def pulse(beep_num, speed):
+    my_speed = speed_func(speed, 120)
+    #time.sleep(my_speed)
+    #time.sleep(my_speed)
+    #playsound(f"beep{beep_num}.mp3") # beep1.mp3 if beep_num is 1
+    #winsound.PlaySound(f"beep{beep_num}.wav", winsound.SND_ASYNC)
+    channel2.play(beep2, loops = -1)
+
+while True:
+    my_speed1 = speed_func(2, 120)
+    my_speed2 = speed_func(2, 120)
+    pulse(1, 2)
+    #pulse(2, 3)
+    time.sleep(.5)
 
 
 
 
+while True:   
+    threading.Thread(target=pulse, args=(frequency1, .5)).start()
+    threading.Thread(target=pulse, args=(frequency2, .5)).start()
+    thread_pulse1 = threading.Thread(target=pulse, args=(1, 3,))
+    thread_pulse2 = threading.Thread(target=pulse, args=(2, 2,))
+    thread_pulse1.start()
+    thread_pulse2.start()
 
-
-
-
-# while True:
-    
-# threading.Thread(target=pulse, args=(frequency1, .5)).start()
-# threading.Thread(target=pulse, args=(frequency2, .5)).start()
-#     thread_pulse1 = threading.Thread(target=pulse, args=(1, 3,))
-#     thread_pulse2 = threading.Thread(target=pulse, args=(2, 2,))
-#     thread_pulse1.start()
-#     thread_pulse2.start()
-
-#     thread_pulse1.join()
-#     thread_pulse2.join()
+    thread_pulse1.join()
+    thread_pulse2.join()
 
 
 # alternative theory: making a timeline and placing beats within that
 
-# def main():
-#     beat = 12
-#     beats = [
-#         (800, 6),
-#         (1000, 6)
-#     ]
+def main():
+    beat = 12
+    beats = [
+        (800, 6),
+        (1000, 6)
+    ]
 
-#     def play_sound(sound_list, t):
-#         to_play = 0
-#         for sound in sound_list:
-#             freq, time = sound
-#             if t % time == 0:
-#                 to_play += freq
-#         if to_play != 0:
-#             winsound.Beep(to_play, 100)
+    def play_sound(sound_list, t):
+        to_play = 0
+        for sound in sound_list:
+            freq, time = sound
+            if t % time == 0:
+                to_play += freq
+        if to_play != 0:
+            winsound.Beep(to_play, 100)
 
-#     for i in range(1):
-#         for t in range(beat):
-#             play_sound(beats, t)
-#             time.sleep(1/beat)
+    for i in range(1):
+        for t in range(beat):
+            play_sound(beats, t)
+            time.sleep(1/beat)
 
-# if __name__== "__main__":
-#     main()
+if __name__== "__main__":
+    main()
